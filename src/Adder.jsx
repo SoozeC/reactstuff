565f76b015e54246daf48ab5b469a4f406ba9bbc
Suzanne <sl_clark@yahoo.co.uk>
2018-06-06 17:18:05 +0100

    adder added

---
+++ src/Adder.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class Adder extends Component {
+
+	constructor(props) {
+		super(props);
+			this.state = { 
+				arrayOfNums: [],
+				total: 0,
+				enteredNum: 0
+				};
+		this.handleUpdate = this.handleUpdate.bind(this);
+		this.handleAdd = this.handleAdd.bind(this);
+		this.handleTotal = this.handleTotal.bind(this);
+	}
+
+	handleUpdate(e){
+		this.setState( { enteredNum: +e.target.value } )
+	}
+
+	handleAdd(){
+		let { arrayOfNums, enteredNum } = this.state;
+		this.setState( { arrayOfNums: arrayOfNums.concat([enteredNum]) } )
+	}
+
+	handleTotal() {
+		let { arrayOfNums } = this.state;
+		this.setState( { total: arrayOfNums.reduce((totes, item) => totes += item, 0 )} )
+	}
+
+	render() {
+		return ( 
+			<React.Fragment>
+				<input onChange={ this.handleUpdate } ></input> 
+				<button onClick={ this.handleAdd } >Add another number</button>
+				<button onClick={ this.handleTotal } >Total all numbers</button>
+				<ul>
+					{this.state.arrayOfNums.map((item, index) => <li key={index}>{item} </li>)}
+				</ul>
+				<p>{this.state.total}</p>
+			</React.Fragment>
+		)
+	};
+}
+
+export default Adder;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import List from './List';
-// import List from './List';
+import Adder from './Adder';
 
-// let names = ['name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7'];
 
 const App = () => (
-	<List />
+	<Adder />
 );
 
 export default App;
