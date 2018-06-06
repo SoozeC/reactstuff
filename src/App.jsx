36010f016113a33bbd93a48e89b6d5b06f57479c
Suzanne <sl_clark@yahoo.co.uk>
2018-06-06 09:29:53 +0100

    more changes that haven't been saved apparently

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 // import Clicked from './Clicked';
 // import Square from './Square';
 // import Toggletext from './Toggletext';
-import Counter from './Counter';
+// import Counter from './Counter';
+import StepCounter from './StepCounter';
 
 const App = () => (
-	<Counter initial={ 50 } max={ 100 } />
+	<StepCounter initial={ 50 } max={ 100 } />
 );
 
 export default App;
